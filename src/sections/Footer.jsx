ed9fbb901b5016786e3f7d842997595e9f1f705f
Nao077 <naomiambassa7@gmail.com>
2025-07-20 15:31:01 +0100

    change Footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -43,7 +43,7 @@ const Footer = () => {
       <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
           <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
               <img src={copyrightSign} alt="copy right sign" width={20} height={20} className="rounded-full m-0" />
-              <p>Copyright. All rights reserved.</p>
+              <p>Copyright 2025 © Victoire Ambassa. All rights reserved.</p>
 
           </div>
           <p className="font-montserrat cursor-pointer">Terms & Conditions</p>
